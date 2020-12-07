f46e5032a596496c8aeeb9139257d62045c61bbb
guosen <guosen@sglcapital.com.cn>
2020-12-07 18:07:46 +0800

    git commit -m "first commit"

---
+++ gw/src/main/java/com/guosen/gw/ConsulApp.java
@@ -0,0 +1,17 @@
+package com.guosen.gw;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication  
+@EnableDiscoveryClient  
+@RestController  
+public class ConsulApp {  
+      
+    @RequestMapping("/health")  
+    public Object home() {  
+        return "gw";  
+    }
+}
